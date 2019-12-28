6f05212cf1e12528ccdbebb5be61de18adafdb6f
zenith <1477363426@qq.com>
2019-12-28 20:14:53 +0800

    添加选择界面框架

---
+++ app/src/main/java/com/zenithweather/android/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.zenithweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+    public  static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client  = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+        //利用OkHttp封装类对服务器进行交互。
+        //发起一条http请求只需要调用sendokhttprequest()方法，传入请求地址，并注册一个回调来处理服务器响应。
+    }
+}
---
+++ app/src/main/java/com/zenithweather/android/util/Utility.java
@@ -0,0 +1,84 @@
+package com.zenithweather.android.util;
+
+import android.text.TextUtils;
+
+import com.zenithweather.android.database.City;
+import com.zenithweather.android.database.Country;
+import com.zenithweather.android.database.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+    /*
+    解析和处理服务器返回的省级数据
+     */
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+           try{
+               JSONArray allProvinces =  new JSONArray(response);
+               for(int i = 0; i < allProvinces.length();i++){
+                   JSONObject provinceObject = allProvinces.getJSONObject(i);
+                   Province province = new Province();
+                   province.setProvinceName(provinceObject.getString("name"));
+                   province.setProvinceCode(provinceObject.getInt("id"));
+                   province.save();
+               }
+               return true;
+           }catch (JSONException e){
+               e.printStackTrace();
+           }
+        }
+        return false;
+    }
+    /*
+     解析和处理服务器返回的市级数据
+     */
+    public static boolean handleCityResponse(String response, int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCities =  new JSONArray(response);
+                for(int i = 0; i < allCities.length();i++){
+                    JSONObject provinceObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(provinceObject.getString("name"));
+                    city.setCityCode(provinceObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /*
+     解析和处理服务器返回的县级数据
+     */
+    public static boolean handleCountryResponse(String response,int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                /*
+                先使用JSONArray和JSONObject将数据解析出来，然后再组装成实体类对象，再调用save()方法将数据
+                存储到数据库当中。对于上面另外两个类也是同样的解析方法。
+                 */
+                JSONArray allCountries =  new JSONArray(response);
+                for(int i = 0; i < allCountries.length(); i++){
+                    JSONObject countryObject = allCountries.getJSONObject(i);
+                    Country country = new Country();
+                    country.setCountryName(countryObject.getString("name"));
+                    country.setWeatherId(countryObject.getString("weather_id"));
+                    country.setCityId(cityId);
+                    country.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+}
