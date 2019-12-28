bc72fe9d4849c585037b853b7ca9f8727bee1693
zenith <1477363426@qq.com>
2019-12-29 00:54:39 +0800

    编写预报布局

---
+++ app/src/main/java/com/zenithweather/android/gson/Now.java
@@ -0,0 +1,18 @@
+package com.zenithweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperatrue;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt")
+        public String info;
+
+    }
+}
