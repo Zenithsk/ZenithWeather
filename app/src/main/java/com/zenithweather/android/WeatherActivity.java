2bf52c3f679a93aa4df4e5abaf8623083432176d
zenith <1477363426@qq.com>
2019-12-29 17:37:07 +0800

    增加后台更新

--- app/src/main/java/com/zenithweather/android/WeatherActivity.java
+++ app/src/main/java/com/zenithweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.zenithweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.Image;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.zenithweather.android.gson.Forecast;
 import com.zenithweather.android.gson.Weather;
+import com.zenithweather.android.service.AutoUpdateService;
 import com.zenithweather.android.util.HttpUtil;
 import com.zenithweather.android.util.Utility;
 
@@ -185,6 +187,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startService(intent);
     }
     /*
     加载bing每日图片
